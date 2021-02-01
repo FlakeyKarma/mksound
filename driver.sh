221a6c798de3c11bf47a93b25bc9eb7476f13bb0
FlakeyKarma <flakeykarma21@gmail.com>
2021-02-01 15:22:13 -0700

    Commented out the sound output to make it opitonal as frequency and duration output and made the loop a true 10 incrementation loop.

--- driver.sh
+++ driver.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-for i in {1..10}; do
-	./bin/python2.7 mksound.py > "SOUND_LIST_$(date  | cut -d' ' -f5,6,7)$i.txt";
+for i in {0..10}; do
+	echo "Starting $i";
+	./bin/python2.7 mksound.py # > "SOUND_LIST_$(date  | cut -d' ' -f5,6,7)$i.txt";
 	lame -b 320 OUTPUT.wav 1>/dev/null 2>/dev/null;
 	mpg123 OUTPUT.mp3 1>/dev/null 2>/dev/null;
 	echo "Done with $i";
